a8e30640c19943a7ed019cf09aa82826ad232444
Daniel Holguin <doholguin@miners.utep.edu>
2022-08-31 11:36:45 -0600

    update

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Arrow = a, Chars = c) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -31,6 +31,8 @@ int main()
       break;
     case 'a':
       puts("You selected arrow:");
+      print_triangle(5,7);
+      print_square(5,7);
       break;
     case 'q':
       puts("Bye!");
